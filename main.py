abf6c7c58f3cefdbe555ee507968aefd786a3379
Jakub <jakub.wiecek@outlook.com>
2021-08-18 22:37:35 +0200

    vesrion 1.1

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
-ROOT_HEIGHT = 400
-ROOT_WIDTH = 600
+ROOT_HEIGHT = 500
+ROOT_WIDTH = 800
 BG_COLOR = "bisque"
 FONT = "ARIAL"
 
@@ -19,188 +19,229 @@ class App:
         self.root = tkinter.Tk()
         self.root.geometry(str(ROOT_WIDTH) + 'x' + str(ROOT_HEIGHT))
         self.root.resizable(width=False, height=False)
+        self.root.title("Automatyczne Wyłączanie PC")
         self.root.configure(bg=BG_COLOR)
-        self.labelTitle = Label(self.root, text="AUTOMATYCZNE WYLĄCZANIE KOMPUTERA", font=(FONT, 19), bg=BG_COLOR)
+
+        # title
+        self.labelTitle = Label(self.root, text="AUTOMATYCZNE WYŁĄCZANIE KOMPUTERA", font=(FONT, 19), bg=BG_COLOR)
         self.labelTitle.place(relx=0.5, rely=0.08, anchor=CENTER)
-        self.buttonExit = Button(self.root, text="Wyjście", pady=15, width=20, fg="white", bg="green",
+
+        self.labelAuthor = Label(self.root, text="Autor: Jakub Więcek", font=(FONT, 10), bg=BG_COLOR)
+        self.labelAuthor.place(x=650, y=470)
+        # buttons
+        self.buttonRun = Button(self.root, text="Uruchom odliczanie", pady=15, width=22, fg="white", bg="green",
+                                command=self.set_time_mode)
+        self.buttonRun.place(x=600, y=100)
+
+        self.buttonStop = Button(self.root, text="Anuluj odliczanie", pady=15, width=22, fg="white", bg="green",
+                                 command=self.stop_counter)
+        self.buttonStop.place(x=600, y=200)
+
+        self.buttonExit = Button(self.root, text="Wyjście", pady=15, width=22, fg="white", bg="green",
                                  command=self.quit)
-        self.buttonExit.place(x=50, y=320)
+        self.buttonExit.place(x=600, y=300)
 
-    def quit(self):
-        self.root.destroy()
-
-
-class InputWindow(App):
-    def __init__(self):
-        super().__init__()
-        self.timeChoice = IntVar()
-
-        Radiobutton(self.root, text="WPISZ CZAS DO WYLĄCZENIA KOMPUTERA", variable=self.timeChoice, value=0,
-                    bg=BG_COLOR).place(x=15, y=80)
-
-        self.buttonRun = Button(self.root, text="Włącz odliczanie", pady=15, width=22, fg="white", bg="green",
-                                command=self.click)
-        self.buttonRun.place(x=380, y=320)
-        self.CountObject = None
-        self.InputObject = None
-        self.labelHour1 = Label(self.root, text="GODZIN: ", bg=BG_COLOR)
-        self.labelHour1.place(x=15, y=ROOT_HEIGHT / 3)
-        self.entryHour1 = Entry(self.root, width=5, borderwidth=3)
-        self.entryHour1.insert(0, "0")
-        self.entryHour1.place(x=ROOT_WIDTH / 6.5, y=ROOT_HEIGHT / 3)
-        self.labelMinute1 = Label(self.root, text="MINUT: ", bg=BG_COLOR).place(x=ROOT_WIDTH / 4, y=ROOT_HEIGHT / 3)
-        self.entryMinute1 = Entry(self.root, width=5, borderwidth=3)
-        self.entryMinute1.insert(0, "0")
-        self.entryMinute1.place(x=ROOT_WIDTH / 2.9, y=ROOT_HEIGHT / 3)
-
+        # radio buttons
         MODES = ["Twarde wyłączenie komputera", "Wyłącz komputer", "Uruchom Ponownie", "Wyloguj", "Hibernacja"]
         self.modeChoice = IntVar()
         i = 0
         for text in MODES:
             Radiobutton(self.root, text=text, variable=self.modeChoice, value=i, bg=BG_COLOR) \
-                .place(x=ROOT_WIDTH / 1.5, y=120 + i * 25)
+                .place(x=350, y=100 + i * 25)
             i += 1
 
-        Radiobutton(self.root, text="WPISZ GODZINĘ WYLĄCZENIA KOMPUTERA", variable=self.timeChoice, value=1,
-                    bg=BG_COLOR).place(x=15, y=200)
-        self.labelHour2 = Label(self.root, text="GODZIA: ", bg=BG_COLOR).place(x=15, y=240)
+        # entry 1
+        self.timeMode = IntVar()
+        Radiobutton(self.root, text="WPISZ CZAS DO WYLĄCZENIA KOMPUTERA", variable=self.timeMode, value=0,
+                    bg=BG_COLOR).place(x=15, y=80)
+
+        self.labelHour1 = Label(self.root, text="GODZIN: ", bg=BG_COLOR)
+        self.labelHour1.place(x=15, y=120)
+        self.entryHour1 = Entry(self.root, width=5, borderwidth=3)
+        self.entryHour1.insert(0, "1")
+        self.entryHour1.place(x=90, y=120)
+
+        self.labelMinute1 = Label(self.root, text="MINUT: ", bg=BG_COLOR).place(x=140, y=120)
+        self.entryMinute1 = Entry(self.root, width=5, borderwidth=3)
+        self.entryMinute1.insert(0, "0")
+        self.entryMinute1.place(x=205, y=120)
+
+        # entry 2
+        Radiobutton(self.root, text="WPISZ GODZINĘ WYLĄCZENIA KOMPUTERA", variable=self.timeMode, value=1,
+                    bg=BG_COLOR).place(x=15, y=170)
+
+        self.labelHour2 = Label(self.root, text="GODZINA: ", bg=BG_COLOR).place(x=15, y=200)
         self.entryHour2 = Entry(self.root, width=5, borderwidth=3)
-        self.entryHour2.insert(0, "0")
-        self.entryHour2.place(x=93, y=240)
-        self.labelMinute2 = Label(self.root, text="MINUTA: ", bg=BG_COLOR).place(x=150, y=240)
+        self.entryHour2.insert(0, "1")
+        self.entryHour2.place(x=90, y=200)
+
+        self.labelMinute2 = Label(self.root, text="MINUTA: ", bg=BG_COLOR).place(x=140, y=200)
         self.entryMinute2 = Entry(self.root, width=5, borderwidth=3)
         self.entryMinute2.insert(0, "0")
-        self.entryMinute2.place(x=220, y=240)
-        self.root.mainloop()
+        self.entryMinute2.place(x=205, y=200)
 
-    def click(self):
-        if self.timeChoice.get() == 0:
-            return self.click1()
-        if self.timeChoice.get() == 1:
-            self.click2()
-
-    def click1(self):
-        try:
-            hour = int(self.entryHour1.get())
-            minute = int(self.entryMinute1.get())
-            if hour < 0 or minute < 0 or (hour == 0 and minute == 0) or hour > 23 or minute > 59:
-                messagebox.showwarning(title="Błąd", message="Wpisaną błędną wartość")
-                return
-
-            self.root.destroy()
-            self.CountObject = Couting(hour, minute, 0, self.modeChoice.get(), self.InputObject)
-        except ValueError:
-            messagebox.showwarning(title="Błąd", message="Podana wartość nie jest liczba")
-            return
-
-    def click2(self):
-        try:
-            hour = int(self.entryHour2.get())
-            minute = int(self.entryMinute2.get())
-            if hour < 0 or minute < 0 or (hour == 0 and minute == 0) or hour > 23 or minute > 59:
-                messagebox.showwarning(title="Błąd", message="Wpisaną błędną wartość")
-                return
-            self.root.destroy()
-            self.CountObject = Couting(hour, minute, 1, self.modeChoice.get(), self.InputObject)
-        except ValueError:
-            messagebox.showwarning(title="Błąd", message="Podana wartość nie jest liczba")
-            return
-
-    def __del__(self):
-        print("Usunieto input_window")
-
-    def loop(self):
-        self.root.mainloop()
-
-
-class Couting(App):
-    def __init__(self, hour, minute, timeChoice, modeChoice, inputObject):
-        super().__init__()
-        self.InputObject = inputObject
-        self.timeNow = datetime.now()
-        self.timeEnd = IntVar()
-        self.modeChoice = modeChoice
+        # count_down
+        self.isActiveCountDown = False
+        self.timeNow = None
+        self.timeEnd = None
         self.drawTimeToEndActive = None
-        # 0 oznacza czas za ile zostanie wyłączony komputer a 1 oznacza godzinę o której zostanie wyłączony komputer
-        self.timeChoice = timeChoice
-        if self.timeChoice == 0:
+
+        self.labelCounterTitle = Label(self.root, text="", font=(FONT, 15), bg=BG_COLOR)
+        self.labelCounter = Label(self.root, text=self.remaining_time_to_end(), font=(FONT, 25), bg=BG_COLOR)
+
+        self.labelHourEndTitle = Label(self.root, text="Godzina wykonania operacji: ", font=(FONT, 15), bg=BG_COLOR)
+        self.labelHourEnd = Label(self.root, text="", font=(FONT, 25), bg=BG_COLOR)
+
+    def set_time_end(self, hour, minute):
+        self.timeNow = datetime.now()
+        if self.timeMode.get() == 0:
             self.timeEnd = self.timeNow + timedelta(hours=hour, minutes=minute)
-        if self.timeChoice == 1:
+        elif self.timeMode.get() == 1:
             self.timeEnd = self.timeNow
             while self.timeEnd.hour != hour or self.timeEnd.minute != minute:
                 self.timeEnd = self.timeEnd + timedelta(minutes=1)
             self.timeEnd = self.timeEnd - timedelta(seconds=self.timeNow.second)
 
-        self.buttonStop = Button(self.root, text="Anuluj odliczanie", padx=30, pady=15, fg="white", bg="green",
-                                 command=self.back_to_input_window)
-        self.buttonStop.place(x=ROOT_WIDTH / 1.5, y=320)
-        self.draw_time_to_end()
+    def set_time_mode(self):
+        if self.isActiveCountDown:
+            messagebox.showwarning(title="Błąd", message="Musisz anulować bieżące odliczanie")
+            return
 
-        Label(self.root, text="Pozostały czas do " + self.operation_translate() + ": ",
-              font=(FONT, 15), bg=BG_COLOR).place(x=25, y=100)
+        if self.timeMode.get() == 0:
+            self.activate_countdown_to_time()
+        elif self.timeMode.get() == 1:
+            self.activate_countdown_to_hour()
 
-        Label(self.root, text="Operacja zostanie wykonana o godzinie: ",
-              font=(FONT, 15), bg=BG_COLOR).place(x=25, y=175)
-        self.draw_end_time()
+    # aktywuje odliczanie jeśli wpiszemy za ile chcemy wyłączyć pc
+    def activate_countdown_to_time(self):
+        try:
+            hour = int(self.entryHour1.get())
+            minute = int(self.entryMinute1.get())
+            if hour < 0 or minute < 0 or (hour == 0 and minute == 0) or hour > 23 or minute > 59:
+                messagebox.showwarning(title="Błąd", message="Wpisano błędną wartość")
+                return
+            self.isActiveCountDown = True
+            self.set_time_end(hour, minute)
+            self.draw_counter()
+            self.draw_end_hour_and_title()
+            self.draw_counter_title()
+            if self.modeChoice.get() == 4:
+                messagebox.showwarning(title="Uwaga",
+                                       message="Jeśli hibernacja w systemie nie jest włączona, operacja nie zakończy się pomyślnie")
 
-    def back_to_input_window(self):
-        self.root.destroy()
+        except ValueError:
+            messagebox.showwarning(title="Błąd", message="Podana wartość nie jest liczba")
+            return
+
+    # aktywuje odliczanie jeśli wpiszemy o której chcemy wyłączyć pc
+    def activate_countdown_to_hour(self):
+        try:
+            hour = int(self.entryHour2.get())
+            minute = int(self.entryMinute2.get())
+            if hour < 0 or minute < 0 or (hour == 0 and minute == 0) or hour > 23 or minute > 59:
+                messagebox.showwarning(title="Błąd", message="Wpisano błędną wartość")
+                return
+            self.isActiveCountDown = True
+            self.set_time_end(hour, minute)
+            self.draw_counter()
+            self.draw_end_hour_and_title()
+            self.draw_counter_title()
+            if self.modeChoice.get() == 4:
+                messagebox.showwarning(title="Uwaga",
+                                       message="Jeśli hibernacja w systemie nie jest włączona, operacja nie zakończy się pomyślnie")
+
+        except ValueError:
+            messagebox.showwarning(title="Błąd", message="Podana wartość nie jest liczba")
+            return
+
+    def stop_counter(self):
+        if not self.isActiveCountDown:
+            return
+        self.isActiveCountDown = False
+        self.timeEnd = None
+        self.timeNow = None
         self.root.after_cancel(self.drawTimeToEndActive)
-        InputWindow()
+        self.drawTimeToEndActive = None
 
-    def draw_end_time(self):
+        self.labelCounterTitle.place_forget()
+        self.labelCounter.place_forget()
+        self.labelHourEndTitle.place_forget()
+        self.labelHourEnd.place_forget()
+
+    def __del__(self):
+        print("Usunieto input_window")
+
+    def quit(self):
+        self.root.destroy()
+
+    def draw_end_hour_and_title(self):
+        self.labelHourEndTitle = Label(self.root, text="Godzina wykonania operacji: ", font=(FONT, 15), bg=BG_COLOR)
+        self.labelHourEndTitle.place(x=15, y=360)
         hour = self.timeEnd.hour
-        min = self.timeEnd.minute
-        lab = Label(self.root, text=str(hour).zfill(2) + ":" + str(min).zfill(2), font=(FONT, 25), bg=BG_COLOR)
-        lab.place(x=25, y=200)
+        minutes = self.timeEnd.minute
+        self.labelHourEnd = Label(self.root, text=str(hour).zfill(2) + ":" + str(minutes).zfill(2), font=(FONT, 25),
+                                  bg=BG_COLOR)
+        self.labelHourEnd.place(x=15, y=390)
 
-    def draw_time_to_end(self):
-        lab = Label(self.root, text=self.remaining_time(), font=(FONT, 25), bg=BG_COLOR)
-        lab.place(x=25, y=125)
-        self.drawTimeToEndActive = self.root.after(1000, self.draw_time_to_end)
+    def draw_counter_title(self):
+        self.labelCounterTitle.place_forget()
+        self.labelCounterTitle = Label(self.root, text="Czas do " + str(self.operation_translate()) + ": ",
+                                       font=(FONT, 15), bg=BG_COLOR)
+        self.labelCounterTitle.place(x=15, y=280)
 
-    def remaining_time(self):
+    def draw_counter(self):
+        if not self.isActiveCountDown:
+            self.root.after_cancel(self.drawTimeToEndActive)
+            self.labelCounter.place_forget()
+            return
+        self.labelCounter.place_forget()
+        self.labelCounter = Label(self.root, text=self.remaining_time_to_end(), font=(FONT, 25), bg=BG_COLOR)
+        self.labelCounter.place(x=15, y=310)
+        self.drawTimeToEndActive = self.root.after(1000, self.draw_counter)
+
+    def remaining_time_to_end(self):
+        if not self.isActiveCountDown:
+            return
         now = datetime.now()
-
         if self.timeEnd > now:
-            result = self.timeEnd - timedelta(hours=now.hour, minutes=now.minute,
-                                              seconds=now.second)
+            result = self.timeEnd - timedelta(hours=now.hour, minutes=now.minute, seconds=now.second)
+            if result.hour == 0 and result.minute == 1 and result.second == 0:
+                messagebox.showwarning(title="Uwaga",
+                                       message="Do " + str(self.operation_translate()) + " pozostała 1 minuta!")
 
             return str(result.hour).zfill(2) + ":" + str(result.minute).zfill(2) + ":" + str(result.second).zfill(2)
         else:
-            self.root.destroy()
             self.operation()
 
     def operation(self):
-        if self.modeChoice == 0:
+        self.root.destroy()
+        if self.modeChoice.get() == 0:
             os.system("shutdown /p /f")
-        elif self.modeChoice == 1:
-            os.system("shutdown /s")
-        elif self.modeChoice == 2:
+        elif self.modeChoice.get() == 1:
+            os.system("shutdown /s ")
+        elif self.modeChoice.get() == 2:
             os.system("shutdown /r -t 0")
-        elif self.modeChoice == 3:
+        elif self.modeChoice.get() == 3:
             os.system("shutdown /l")
-        elif self.modeChoice == 4:
+        elif self.modeChoice.get() == 4:
             os.system("shutdown /h")
 
     def operation_translate(self):
-        if self.modeChoice == 0:
+        if self.modeChoice.get() == 0:
             return "wyłączenia komputera"
-        elif self.modeChoice == 1:
+        elif self.modeChoice.get() == 1:
             return "wyłączenia komputera"
-        elif self.modeChoice == 2:
+        elif self.modeChoice.get() == 2:
             return "uruchomienia ponownie"
-        elif self.modeChoice == 3:
-            return "wylogowanie"
-        elif self.modeChoice == 4:
+        elif self.modeChoice.get() == 3:
+            return "wylogowania"
+        elif self.modeChoice.get() == 4:
             return "hibernacji"
-
-    def __del__(self):
-        print("Usunieto counting_window")
 
 
 def main():
-    A = InputWindow()
+    A = App()
+    A.root.mainloop()
 
 
 if __name__ == "__main__":
